ed36a9ca0b916f004dc9099d6221ae85c95fb337
Eaz1424 <111675362+Eaz1424@users.noreply.github.com>
2022-12-01 19:16:52 +0530

    Create IntegerVsDecimalCheck.cpp

---
+++ IntegerVsDecimalCheck.cpp
@@ -0,0 +1,32 @@
+#include <iostream> 
+
+
+
+
+using namespace std;
+
+
+int main()
+{
+    float roll;
+    cout<<"Enter your roll number";
+    cin>>roll;
+    
+    if((int)roll==roll)
+    {
+        cout<<"Valid Roll No"<<endl;
+
+    }
+else
+{
+   
+        cout<<"Invalid Roll No.";
+    
+}
+
+
+
+
+
+return 0;
+}  
